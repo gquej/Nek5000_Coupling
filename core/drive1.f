fa33087ee09ea61dcd2c800efcecf9d68ddd02a2
Guillaume <guillaume.querinjean@uclouvain.be>
2024-10-01 22:55:22 +0200

    1st try to construct the matrix to impose the vorticity, in 2D, within drive1

--- core/drive1.f
+++ core/drive1.f
@@ -178,6 +178,25 @@ c-----------------------------------------------------------------------
       include 'TSTEP'
       include 'INPUT'
       include 'CTIMER'
+      include 'GEOM'
+      include 'DXYZ'
+      integer e,i,j,ifc
+      common /nekcb/ cbcmesh
+      character cb*3
+
+      real nx,ny
+      real mat_sn(2*lx1,2*lx1,lelt)
+      real uart, vart, omart
+      real temp1, temp2
+
+      real u_,v_,p_,om_,du_
+      real x_,y_
+
+      !variables for the new implementation
+      integer tmpl1(2*ldim,nelv), tmpl2(2*ldim,nelv)
+      integer tmpl3(2*ldim,nelv), tmpl4(2*ldim,nelv)
+      integer n_fbc, n_fnbc
+      integer l_fbc(nelv), l_fnbc(nelv)
 
       call nekgsync()
 
@@ -203,6 +222,194 @@ c-----------------------------------------------------------------------
       msteps = 1
 
       irstat = int(param(120))
+
+      !assemble the matrix to impose vorticity BC
+      nx = -1.0
+      ny = 0.
+
+      do e = 1,nelt 
+        !first, find the faces that have 'v' BC's, if any
+        n_fbc = 0! number of faces that have a BC
+        n_fnbc = 0! number of faces that do not have a BC
+
+        cb = cbc(4,e,1) ! face x-
+        if(cb.eq.'v  ') then 
+          n_fbc = n_fbc + 1
+          tmpl1(n_fbc,e) = 4
+          tmpl3(4,e) = 1
+        else 
+          n_fnbc = n_fnbc + 1
+          tmpl2(n_fnbc,e) = 4
+          tmpl3(4,e) = 0
+        endif 
+
+        cb = cbc(2,e,1) ! face x+
+        if(cb.eq.'v  ') then 
+          n_fbc = n_fbc + 1
+          tmpl1(n_fbc,e) = 2
+          tmpl3(2,e) = 1
+        else 
+          n_fnbc = n_fnbc + 1
+          tmpl2(n_fnbc,e) = 2
+          tmpl3(2,e) = 0
+        endif
+
+        cb = cbc(1,e,1) ! face y-
+        if(cb.eq.'v  ') then 
+          n_fbc = n_fbc + 1
+          tmpl1(n_fbc,e) = 1
+          tmpl3(1,e) = 1
+        else 
+          n_fnbc = n_fnbc + 1
+          tmpl2(n_fnbc,e) = 1
+          tmpl3(1,e) = 0
+        endif
+
+        cb = cbc(3,e,1) ! face y+
+        if(cb.eq.'v  ') then 
+          n_fbc = n_fbc + 1
+          tmpl1(n_fbc,e) = 3
+          tmpl3(3,e) = 1
+        else 
+          n_fnbc = n_fnbc + 1
+          tmpl2(n_fnbc,e) = 3
+          tmpl3(0,e) = 0
+        endif
+        ! now we also put the faces w/out BC in tmpl1
+        do i = 1, n_fnbc
+          tmpl1(n_fbc + i,e) = tmpl2(i,e)
+        enddo
+        ! and in tmpl2, we put 1 if there's a BC and 0 otherwise
+        do i = 1, n_fbc
+          tmpl2(i,e) = 1
+        enddo
+        do i = 1, n_fnbc
+          tmpl2(n_fbc+i,e) = 0
+        enddo
+        l_fbc(e) = n_fbc
+        l_fnbc(e) = n_fnbc
+        ! we also write the inverse maping of tmpl1 in tmpl4
+        do i = 1, 2*ldim 
+         tmpl4(tmpl1(i,e),e) = i 
+        enddo 
+        print *, tmpl1(1,e),tmpl1(2,e),tmpl1(3,e),tmpl1(4,e)
+        print *, tmpl4(1,e),tmpl4(2,e),tmpl4(3,e),tmpl4(4,e)
+      enddo
+
+      ! now, we build the matrix, side by side
+      ! do e = 1, nelv 
+      !    ! first, init the matrix to 0
+      !    do i = 1, 2*lx1*2*ldim 
+      !       do j = 1, 2*lx1*2*ldim 
+      !          mat_sn(i,j,e) = 0.
+      !       enddo 
+      !    enddo 
+
+      !    do ifc = 1, l_fbc(e)
+      !       if(tmpl1(ifc,e).eq.4) then
+      !          !first point: might impact variables of side 1 if they are unknowns
+      !          if(tmpl2(tmpl4(1,e),e).eq.1) then 
+      !             ehexto8hex
+      !          else 
+      !             ehexto8hex
+      !          endif
+      !          !then do loop on point 2, lx1-1
+      !          do i = 2, lx1-1
+      !             e 
+      !          enddo
+      !          !last point: might impact variables of side 3 if they are unknowns
+      !          last point
+      !       else if () then 
+
+      !       else if () then 
+
+      !       else if () then 
+
+      !       endif
+
+
+      !    enddo
+
+      ! enddo
+
+
+
+
+      do e = 1,nelt 
+         do i = 1,lx1
+            do j = 1, lx1 
+               mat_sn(i,j,e) = -sym1(1,i,1,e)*dytm1(j,i)
+               mat_sn(i,j+lx1,e) = sxm1(1,i,1,e)*dytm1(j,i)
+            enddo
+            mat_sn(i,i,e)    = -rym1(1,i,1,e)*dxm1(1,1) - 
+     &       sym1(1,i,1,e)*dytm1(i,i)
+            mat_sn(i,i+lx1,e) = rxm1(1,i,1,e)*dxm1(1,1) +
+     &        sxm1(1,i,1,e)*dytm1(i,i)
+            mat_sn(i+lx1,i,e) = nx
+            mat_sn(i+lx1,i+lx1,e) = ny
+         enddo
+
+         call LUdec(mat_sn(1,1,e),2*lx1, persnl(1,e),1e-12)
+         ! call invmat(mat_sn(1,1,e),2*lx1, matsnl(1,1,e), persnl(1,e))
+      enddo
+      !creating an uniform rhs with u = 1, v = 1
+      uart = 1.
+      vart = 1.8792
+      omart = 0.
+
+      do e = 1,nelt
+         do i = 1,lx1
+            temp1 = 0.
+            temp2 = 0.
+            do j = 2,lx1
+               x_ = xm1(j,i,1,e)
+               y_ = ym1(j,i,1,e)
+               call GVLFD(x_,y_,u_,v_, p_, om_, du_, du_, du_, du_)
+               temp1 = temp1 + dxm1(1,j)* v_
+               temp2 = temp2 + dxm1(1,j)* u_
+            enddo
+            x_ = xm1(1,i,1,e)
+            y_ = ym1(1,i,1,e)
+            call GVLFD(x_,y_,u_,v_, p_, om_, du_, du_, du_, du_)
+            rhssnl(i,e) = om_/jacmi((i-1)*lx1+1,e) - 
+     &       rxm1(1,i,1,e)*temp1 + rym1(1,i,1,e)*temp2
+            rhssnl(i+lx1,e) = -u_
+         enddo
+      enddo 
+
+      do e = 1,nelt 
+         ! call solaxb(matsnl(1,1,e),2*lx1, rhssnl(1,e),xsol, persnl(1,e))
+         call LUsolv(mat_sn(1,1,e),2*lx1, rhssnl(1,e), persnl(1,e),
+     &    xsol(1,e))
+      enddo
+
+      do e = 1, nelv
+        if(abs(xm1(1,1,1,e)+0.5).lt.1e-12) then 
+        print *, 'Element ', e, xm1(1,1,1,e), ym1(1,1,1,e)
+        do i = 1, LX1
+            x_ = xm1(1,i,1,e)
+            y_ = ym1(1,i,1,e)
+            call GVLFD(x_,y_,u_,v_, p_, om_, du_, du_, du_, du_)
+            print *, abs(xsol(i,e)-u_), abs(xsol(i+lx1,e)-v_)
+            ! print *, xsol(i,e),u_, xsol(i+lx1,e), v_
+        enddo
+      endif
+      enddo
+
+      print *, 'finding the boundaries'
+
+      do e = 1,nelv 
+         do ifc = 1,4
+            cb = cbc(ifc,e,1)
+            print *, 'Element ', e, 'Face ', ifc, 'BC ', cb
+         enddo
+      enddo
+
+
+
+
+
+
 
       do kstep=1,nsteps,msteps
          call nek__multi_advance(kstep,msteps)
@@ -252,14 +459,14 @@ c-----------------------------------------------------------------------
       call nekgsync
 
       call setup_convect(2) ! Save conv vel
-
+      print *, "before settime", time
       if (iftran) call settime
+      print *, "after settime", time
       if (ifmhd ) call cfl_check
       call setsolv
       call comment
 
       if (ifsplit) then   ! PN/PN formulation
-
          do igeom=1,ngeom
 
          if (ifneknekc .and. igeom.gt.2) then
